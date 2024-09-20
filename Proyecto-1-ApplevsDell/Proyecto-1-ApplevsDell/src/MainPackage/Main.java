69cd0f20bc3474042e6751c8842a193e0c47e178
maduran7 <madurancoin@gmail.com>
2024-09-20 19:36:28 -0400

    home

---
+++ Proyecto-1-ApplevsDell/Proyecto-1-ApplevsDell/src/MainPackage/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package MainPackage;
+
+/**
+ *
+ * @author angel
+ */
+public class Main {
+
+    /**
+     * main method
+     *
+     * @param args
+     */
+
+    public static void main(String[] args) {
+
+        App app = App.getInstance();
+        app.start();
+        
+        
+    }
+
+}
